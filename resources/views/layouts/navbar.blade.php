c258f0abeb09694fb397da0cbb4dd8a80b4a3021
hewerth <hewerth.altamirano@gmail.com>
2020-02-13 20:44:25 -0300

    cambios en el index de la aplicacion

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container d-flex justify-content-between">
-    <a class="navbar-brand" href="/">FONASA</a>
+    <a class="navbar-brand" href="/">HOSPITAL</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
